b3ce8a4738eedc0cc3eea564025f9177bb4404f7
kalel <kalel1130@Gael-G15>
2023-11-04 11:30:17 +0100

    commit after build

--- auth.ts
+++ auth.ts
@@ -22,17 +22,14 @@ export const { auth, signIn, signOut } = NextAuth({
     Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
-          .object({
-            email: z.string().email(),
-            password: z.string().min(6),
-          })
+          .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (!passwordsMatch) return null;
+          if (passwordsMatch) return user;
         }
 
         console.log("Invalid credentials");
